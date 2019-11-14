8ae6aedcc59fb89de5e87a6c09275f84c8afe464
ttan <rockonedege@users.noreply.github.com>
2019-11-14 11:14:32 +0800

    added script generate Readme.md after sorting

---
+++ make.py
@@ -0,0 +1,40 @@
+entries = """
+https://github.com/vuejs/awesome-vue
+https://github.com/vinta/awesome-python
+https://github.com/veggiemonk/awesome-docker
+https://github.com/josephmisiti/awesome-machine-learning
+https://github.com/Hack-with-Github/Awesome-Hacking
+https://github.com/wasabeef/awesome-android-ui
+https://github.com/sindresorhus/awesome-nodejs
+https://github.com/fffaraz/awesome-cpp
+https://github.com/justjavac/awesome-wechat-weapp
+https://github.com/sorrycc/awesome-javascript
+https://github.com/binhnguyennus/awesome-scalability
+https://github.com/awesome-selfhosted/awesome-selfhosted
+https://github.com/jobbole/awesome-python-cn
+https://github.com/bayandin/awesome-awesomeness
+"""
+
+def to_list(entries):
+    items = []
+    for e in entries.split('\n'):
+        if e:
+            title = e.rsplit('/')[-1].capitalize()
+            items.append(f'- [{title}]({e})')
+
+    return '\n'.join(sorted(items))
+
+def main():
+    from pathlib import Path
+
+    text = """
+# awesome-awesome
+An Awesome-* lists of **personal** interests.
+
+""" + to_list(entries)
+
+    readme = Path(__file__).absolute().parent/'README.md'
+    readme.write_text(text, encoding='utf-8')
+
+if __name__ == "__main__":
+    main()
